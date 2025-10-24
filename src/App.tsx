d91959187e518cfb9de0d2c0727befad45f7ad7e
matus1888 <matus1888@gmail.com>
2025-10-24 18:36:41 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { Box } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MainContextProvider } from "./hooks/useMainContext";
 import { StageEnum } from "./constants";
 import { Content, Header } from "./features";
+import WebApp from "@twa-dev/sdk";
+import { useUserInfo } from "./hooks";
 
 export function App() {
   const [stage, setStage] = useState<StageEnum>(StageEnum.MAIN);
   const [startTimes, setStartTimes] = useState<number[]>([]);
   const [currentGroup, setCurrentGroup] = useState<string[] | undefined>();
+  const userInfo = useUserInfo();
+  
+  const isPortrait = screen.orientation.type.startsWith("portrait");
+  useEffect(() => {
+    if (isPortrait) {
+      WebApp.lockOrientation();
+    }
+  }, [isPortrait]);
+
+  if (!userInfo){
+    return null;
+  }
 
   return (
     <MainContextProvider
@@ -20,7 +34,7 @@ export function App() {
         setCurrentGroup,
       }}
     >
-      <Box padding="16px">
+      <Box id="main" paddingX="16px" paddingBottom="16px" paddingTop="10vh">
         <Header />
         <Content />
       </Box>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { App } from "./App.tsx";
 import WebApp from "@twa-dev/sdk";
 
 WebApp.ready();
+WebApp.expand();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
